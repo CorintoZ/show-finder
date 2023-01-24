2ab39df16ec0616db833a3adec8bc0dfaf9f3f41
CorintoZ <david.moreno92@gmail.com>
2023-01-24 23:57:26 +0100

    Add missing test

--- src/app/components/search-wrapper/search-wrapper.component.spec.ts
+++ src/app/components/search-wrapper/search-wrapper.component.spec.ts
@@ -1,17 +1,20 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { TvmazeApiService } from 'src/app/services/tvmaze-api.service';
 
 import { SearchWrapperComponent } from './search-wrapper.component';
 
 describe('SearchWrapperComponent', () => {
   let component: SearchWrapperComponent;
   let fixture: ComponentFixture<SearchWrapperComponent>;
+  const tvMazeServiceSpyObj = jasmine.createSpyObj('tvMazeService', ['getShowResults']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SearchWrapperComponent],
       imports: [HttpClientTestingModule],
+      providers: [{ provide: TvmazeApiService, useValue: tvMazeServiceSpyObj }],
     }).compileComponents();
   });
 
@@ -30,5 +33,16 @@ describe('SearchWrapperComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('#noResults')).toBeTruthy();
+  });
+
+  it('empty array on search results', (done: DoneFn) => {
+    tvMazeServiceSpyObj.getShowResults.and.returnValue(of([]));
+    component.handleQuery('');
+    fixture.detectChanges();
+    // expect(tvMazeServiceSpyObj.getShowResults).toHaveBeenCalled();
+    component.showResults$.subscribe(results => {
+      expect(results.length).toBe(0);
+      done();
+    });
   });
 });
