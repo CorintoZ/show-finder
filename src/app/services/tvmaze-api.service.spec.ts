ffc17f78c330e255ec1f436234b0142e0a3b26fb
CorintoZ <david.moreno92@gmail.com>
2023-01-24 20:23:06 +0100

    Test service

--- src/app/services/tvmaze-api.service.spec.ts
+++ src/app/services/tvmaze-api.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { TvmazeApiService } from './tvmaze-api.service';
 
 describe('TvmazeApiService', () => {
@@ -12,5 +13,20 @@ describe('TvmazeApiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('empty string on init', (done: DoneFn) => {
+    service.query$.asObservable().subscribe(val => {
+      expect(val).toBe('');
+      done();
+    });
+  });
+
+  it('empty array on search results', (done: DoneFn) => {
+    spyOn(service, 'getShowResults').and.returnValue(of([]));
+    service.searchResults().subscribe(results => {
+      expect(results.length).toBe(0);
+      done();
+    });
   });
 });
